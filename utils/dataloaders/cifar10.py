e0aa23c1a1ce886ce567ae38d472440bf0c0cf4f
MarioProjects <maparla@inf.upv.es>
2021-01-07 09:00:37 +0100

    refactor

--- utils/dataloaders/cifar10.py
+++ utils/dataloaders/cifar10.py
@@ -1,11 +1,10 @@
-from torch.utils.data import DataLoader
 import torch
 import os
 import numpy as np
 from torch.utils.data import Dataset
 import albumentations
 
-import utils.dataload as d
+import utils.dataloaders.utils as d
 
 
 class CIFAR10Dataset(Dataset):
@@ -75,37 +74,3 @@ class CIFAR10Dataset(Dataset):
         image = torch.from_numpy(image).float()
 
         return {"image": image, "label": label}
-
-
-def dataset_selector(train_aug, val_aug, args, is_test=False, data_prefix=""):
-
-    if args.dataset == "CIFAR10":
-        if is_test:
-            test_dataset = CIFAR10Dataset(
-                mode="test", transform=val_aug, normalization=args.normalization, data_prefix=data_prefix
-            )
-
-            return DataLoader(
-                test_dataset, batch_size=args.batch_size, shuffle=False, pin_memory=True, drop_last=False
-            )
-
-        train_dataset = CIFAR10Dataset(
-            mode="train", transform=train_aug, normalization=args.normalization, data_prefix=data_prefix
-        )
-        train_loader = DataLoader(
-            train_dataset, batch_size=args.batch_size, pin_memory=True, shuffle=True
-        )
-
-        val_dataset = CIFAR10Dataset(
-            mode="validation", transform=val_aug, normalization=args.normalization, data_prefix=data_prefix
-        )
-        val_loader = DataLoader(
-            val_dataset, batch_size=args.batch_size, pin_memory=True, shuffle=False, drop_last=False
-        )
-
-    else:
-        assert False, f"Unknown dataset '{args.dataset}'"
-
-    print(f"Train dataset len:  {len(train_dataset)}")
-    print(f"Validation dataset len:  {len(val_dataset)}")
-    return train_loader, val_loader
